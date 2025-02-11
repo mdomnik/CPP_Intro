df54c0488b0998c384d8e963ae4e68034dcd37e4
Maciej Domnik <mdomnik.maciej@gmail.com>
2025-02-11 16:12:57 +0100

    fragtrap adjusted

--- cpp03/ex02/src/FragTrap.cpp
+++ cpp03/ex02/src/FragTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: mdomnik <mdomnik@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 14:32:56 by mdomnik           #+#    #+#             */
-/*   Updated: 2025/02/05 14:42:30 by mdomnik          ###   ########.fr       */
+/*   Updated: 2025/02/11 16:06:13 by mdomnik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 #include <iostream>
 
 //default constructor
-FragTrap::FragTrap(void) : ClapTrap()
+FragTrap::FragTrap(void) : ClapTrap("FragTrap")
 {
-	_name = "FragTrap";
-	std::cout << "FragTrap " << _name << " is created! (Default constructor)" << std::endl;
 	_hitPoints = 100;
 	_energyPoints = 100;
 	_attackDamage = 30;
+	std::cout << "FragTrap " << _name << " is created! (Default constructor)" << std::endl;
 }
 
 //nane constructor
@@ -36,36 +35,34 @@ FragTrap::FragTrap(std::string name) : ClapTrap(name)
 FragTrap::FragTrap(const FragTrap &other) : ClapTrap(other)
 {
 	std::cout << "FragTrap " << _name << " is copied! (Copy constructor)" << std::endl;
-	*this = other;
 }
 
+
 //copy assignment constructor
-FragTrap & FragTrap::operator=(const FragTrap &other)
+FragTrap &FragTrap::operator=(const FragTrap &other)
 {
-	std::cout << "FragTrap " << _name << " is assigned! (Copy assignment constructor)" << std::endl;
 	if (this != &other)
+	{
 		ClapTrap::operator=(other);
+		_hitPoints = other._hitPoints;
+		_energyPoints = other._energyPoints;
+		_attackDamage = other._attackDamage;
+	}
+	std::cout << "FragTrap " << _name << " is assigned from " << other._name << "! (Copy assignment operator)" << std::endl;
 	return *this;
 }
 
 // Destructor
 FragTrap::~FragTrap(void)
 {
-	std::cout << "FragTrap " << _name << " destroyed! (Destructor)" << std::endl;
+	std::cout << "FragTrap " << _name << " is destroyed! (Destructor)" << std::endl;
 }
 
 //attack function
 void FragTrap::attack(std::string const &target)
 {
-	if (_energyPoints > 0 && _hitPoints > 0)
-	{
-		std::cout << "FragTrap " << _name << " attacks " << target << " causing " << _attackDamage << " points of damage!" << std::endl;
-		_energyPoints -= 1;
-	}
-	else if (_hitPoints == 0)
-		std::cout << "FragTrap " << _name << "; a destroyed unit can't attack anymore!" << std::endl;
-	else
-		std::cout << "FragTrap " << _name << " is out of energy and can't attack anymore!" << std::endl;
+	std::cout << "FragTrap " << _name << " prepares to attack!" << std::endl;
+	ClapTrap::attack(target);
 }
 
 //highFivesGuys function
--- cpp03/ex02/src/main.cpp
+++ cpp03/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mdomnik <mdomnik@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 11:25:37 by mdomnik           #+#    #+#             */
-/*   Updated: 2025/02/05 15:14:54 by mdomnik          ###   ########.fr       */
+/*   Updated: 2025/02/11 16:11:24 by mdomnik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,35 @@
 #include "../inc/ScavTrap.hpp"
 #include <iostream>
 
+#include "../inc/ClapTrap.hpp"
+#include "../inc/ScavTrap.hpp"
+#include "../inc/FragTrap.hpp"
+#include <iostream>
+
 int main()
 {
+	// ========================== ClapTrap Testing ==========================
 	{
-		std::cout << "\033[1;32m=== ClapTrap Testing Ground ===\033[0m" << std::endl;
-		ClapTrap clappy("clappy");
-			
+		std::cout << "\033[1;32m=== ClapTrap Constructor Testing ===\033[0m" << std::endl;
+		
+		ClapTrap defaultClap;
+		ClapTrap namedClap("Clappy");
+		ClapTrap copyClap(namedClap);
+		ClapTrap assignedClap;
+		assignedClap = namedClap;
+		
+		std::cout << "\033[1;34m=== ClapTrap Functionality Testing ===\033[0m" << std::endl;
+		
+		namedClap.attack("Bandit-01");
+		namedClap.takeDamage(3);
+		namedClap.beRepaired(2);
+		copyClap.attack("Bandit-02");
+		assignedClap.takeDamage(5);
+		assignedClap.beRepaired(4);
+		
+		std::cout << "\033[1;34m=== ClapTrap Testing Ground ===\033[0m" << std::endl;
+		ClapTrap clappy("Clappy");
+		
 		for (int i = 0; i < 7; i++) {
 			clappy.attack("Bandit-01");
 		}
@@ -32,59 +55,95 @@ int main()
 		clappy.takeDamage(3);
 		clappy.beRepaired(3);
 		clappy.takeDamage(5);
-	
+		
 		clappy.attack("Bandit-01");
 		clappy.takeDamage(5);
 		clappy.beRepaired(5);
-		std::cout << "\033[1;31m===Test Concluded===\033[0m" << std::endl;
+		std::cout << "\033[1;31m=== ClapTrap Test Concluded ===\033[0m" << std::endl;
 	}
+		
+	// ========================== ScavTrap Testing ==========================
 	{
-		std::cout << "\033[1;32m=== ScavTrap Testing Ground ===\033[0m" << std::endl;
-		ScavTrap scavvy("scavvy");
-			
-		for (int i = 0; i < 7; i++) {
+		std::cout << "\033[1;32m=== ScavTrap Constructor Testing ===\033[0m" << std::endl;
+		
+		ScavTrap defaultScav;
+		ScavTrap namedScav("Scavvy");
+		ScavTrap copyScav(namedScav);
+		ScavTrap assignedScav;
+		assignedScav = namedScav;
+		
+		std::cout << "\033[1;34m=== ScavTrap Functionality Testing ===\033[0m" << std::endl;
+		
+		namedScav.attack("Bandit-01");
+		namedScav.takeDamage(20);
+		namedScav.beRepaired(10);
+		namedScav.guardGate();
+
+		copyScav.attack("Bandit-02");
+		assignedScav.takeDamage(30);
+		assignedScav.beRepaired(15);
+		assignedScav.guardGate();
+		
+		std::cout << "\033[1;34m=== ScavTrap Testing Ground ===\033[0m" << std::endl;
+		ScavTrap scavvy("Scavvy");
+
+		// Test energy depletion
+		for (int i = 0; i < 5; i++) {
 			scavvy.attack("Bandit-01");
 		}
-			
+		
 		scavvy.takeDamage(50);
 		scavvy.beRepaired(20);
-		scavvy.takeDamage(50);
-		scavvy.attack("Bandit-01");
-		scavvy.beRepaired(50);
-		scavvy.takeDamage(30);
-		scavvy.beRepaired(30);
 		scavvy.guardGate();
-		scavvy.takeDamage(1000);
-	
-		scavvy.attack("Bandit-01");
-		scavvy.takeDamage(50);
-		scavvy.beRepaired(50);
-		scavvy.guardGate();
-		std::cout << "\033[1;31m===Test Concluded===\033[0m" << std::endl;
-	}
-	{
-		std::cout << "\033[1;32m=== FragTrap Testing Ground ===\033[0m" << std::endl;
-		FragTrap fraggy("fraggy");
 			
-		for (int i = 0; i < 7; i++) {
+		scavvy.takeDamage(50);
+		scavvy.attack("Bandit-01");
+		scavvy.beRepaired(50);
+		scavvy.guardGate();
+		
+		std::cout << "\033[1;31m=== ScavTrap Test Concluded ===\033[0m" << std::endl;
+	}
+
+	// ========================== FragTrap Testing ==========================
+	{
+		std::cout << "\033[1;32m=== FragTrap Constructor Testing ===\033[0m" << std::endl;
+
+		FragTrap defaultFrag;
+		FragTrap namedFrag("Fraggy");
+		FragTrap copyFrag(namedFrag);
+		FragTrap assignedFrag;
+		assignedFrag = namedFrag;
+
+		std::cout << "\033[1;34m=== FragTrap Functionality Testing ===\033[0m" << std::endl;
+
+		namedFrag.attack("Bandit-01");
+		namedFrag.takeDamage(50);
+		namedFrag.beRepaired(20);
+		namedFrag.highFivesGuys();
+
+		copyFrag.attack("Bandit-02");
+		assignedFrag.takeDamage(30);
+		assignedFrag.beRepaired(30);
+		assignedFrag.highFivesGuys();
+
+		std::cout << "\033[1;34m=== FragTrap Testing Ground ===\033[0m" << std::endl;
+		FragTrap fraggy("Fraggy");
+
+		for (int i = 0; i < 5; i++) {
 			fraggy.attack("Bandit-01");
 		}
-			
+
 		fraggy.takeDamage(50);
 		fraggy.beRepaired(20);
+		fraggy.highFivesGuys();
+
 		fraggy.takeDamage(50);
 		fraggy.attack("Bandit-01");
 		fraggy.beRepaired(50);
-		fraggy.takeDamage(30);
-		fraggy.beRepaired(30);
 		fraggy.highFivesGuys();
-		fraggy.takeDamage(1000);
-	
-		fraggy.attack("Bandit-01");
-		fraggy.takeDamage(50);
-		fraggy.beRepaired(50);
-		fraggy.highFivesGuys();
-		std::cout << "\033[1;31m===Test Concluded===\033[0m" << std::endl;
-		return (0);
+
+		std::cout << "\033[1;31m=== FragTrap Test Concluded ===\033[0m" << std::endl;
 	}
+
+	return 0;
 }
